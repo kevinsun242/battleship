28a4cedcae5a06c4ce0c7ccf54b4ba932a3fcd71
Kevin Sun <kevinsun@Kevins-MacBook-Pro.local>
2019-02-13 15:36:58 -0500

    first commit

---
+++ lib/battleship/application.ex
@@ -0,0 +1,29 @@
+defmodule Battleship.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      BattleshipWeb.Endpoint
+      # Starts a worker by calling: Battleship.Worker.start_link(arg)
+      # {Battleship.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Battleship.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    BattleshipWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
