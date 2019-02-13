316e90b93e44cf7391a8b1dc4e7be68e26069416
Kevin Sun <kevinsun@Kevins-MacBook-Pro.local>
2019-02-13 16:33:59 -0500

    More setup, added react

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -22,10 +22,13 @@ module.exports = (env, options) => ({
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         }
       },
       {
@@ -34,6 +37,10 @@ module.exports = (env, options) => ({
       }
     ]
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  //plugins: [
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
